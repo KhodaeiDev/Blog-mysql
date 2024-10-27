df6dfbe731f43053a6a4810ae563d62f71b844bf
khodaeidev <khodaei.dev@gmail.com>
2024-10-27 08:48:42 +0330

    Dev Associations in sequelize relations

--- src/db.js
+++ src/db.js
@@ -1,6 +1,11 @@
 const { Sequelize } = require("sequelize");
 const configs = require("./configs");
 
+const Tag = require("./models/Tag");
+const Articles = require("./models/Articles");
+const TagsArticles = require("./models/TagsArticles");
+const User = require("./models/User");
+
 const db = new Sequelize({
   host: configs.db.host,
   database: configs.db.name,
@@ -9,6 +14,28 @@ const db = new Sequelize({
   port: configs.db.port,
   dialect: configs.db.dialect,
   logging: configs.isProductions ? false : console.log,
+});
+
+User.hasMany(Articles, {
+  onDelete: "CASCADE",
+  foreignkey: "author_id",
+});
+
+Articles.blongsTo(User, {
+  foreignkey: "author_id",
+  as: "author",
+});
+
+Tag.belongsToMany(Articles, {
+  through: TagsArticles,
+  onDelete: "CASCADE",
+  foreignkey: "tag_id",
+});
+
+Articles.belongsToMany(Tag, {
+  through: TagsArticles,
+  onDelete: "CASCADE",
+  foreignkey: "article_id",
 });
 
 module.exports = db;
