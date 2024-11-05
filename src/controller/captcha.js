c3150594e67f34d59c684e9de9014f28a4e8a1f7
khodaeidev <khodaei.dev@gmail.com>
2024-11-05 13:49:10 +0330

    Dev Captcha Generation logic

--- src/controller/captcha.js
+++ src/controller/captcha.js
@@ -1,1 +1,19 @@
-exports.get = async () => {};
+const svgCpatcha = require("svg-captcha");
+const redis = require("../redis");
+const uuidv4 = require("uuid").v4;
+
+exports.get = async (req, res, next) => {
+  const captcha = svgCpatcha.create({
+    size: 4,
+    noise: 5,
+  });
+
+  const uuid = uuidv4();
+
+  await redis.set(`captcha:${uuid}`, captcha.text.toLowerCase(), "EX", 60 * 5);
+
+  res.json({
+    uuid,
+    captcha: captcha.data,
+  });
+};
